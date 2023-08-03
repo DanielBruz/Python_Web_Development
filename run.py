78a43c0f51bfa5b832c36ef787c218ae810e0ac5
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-08-03 07:09:13 +0000

    without folder idea

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import flask_app
 
+# kdyz je tento program run.py spusteny jako hlavni, nastavi se promenna __name__ na __main__.
+# Kdybych tento program importoval jako modul v jiném programu, nastavila by se na "run".
 if __name__ == "__main__":
     debug = False
     host = "192.168.20.165"
