da7db893cb1d040987f78f3b2f729b456415fc07
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-08-05 01:09:49 +0200

    change to IP home

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from mdblog.app import flask_app
 
 # kdyz je tento program run.py spusteny jako hlavni, nastavi se promenna __name__ na __main__.
 # Kdybych tento program importoval jako modul v jiném programu, nastavila by se na "run".
-# Níže je uvedena IP adresa mé virtuální linux mašiny 192.168.20.165
+# Níže je uvedena IP adresa mé virtuální linux mašiny 192.168.20.165 (v práci). Doma = 10.0.0.196
 
 if __name__ == "__main__":
     debug = True
-    host = "192.168.20.165"
+    host = "10.0.0.196"
     flask_app.run(host, debug=debug)
